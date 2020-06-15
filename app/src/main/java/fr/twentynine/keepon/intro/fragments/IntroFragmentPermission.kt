8df5e0855b9e4831ca9c3e7c2c248de297c2e99e
TwentyNine78 <twentynine78@protonmail.com>
2020-06-15 18:08:55 +0200

    Upgraded to AppIntro 6.0 with some improvements

--- app/src/main/java/fr/twentynine/keepon/intro/fragments/IntroFragmentPermission.kt
+++ app/src/main/java/fr/twentynine/keepon/intro/fragments/IntroFragmentPermission.kt
@@ -2,7 +2,6 @@ package fr.twentynine.keepon.intro.fragments
 
 import android.content.Context
 import android.content.Intent
-import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
@@ -14,33 +13,33 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
-import com.github.paolorotolo.appintro.ISlideBackgroundColorHolder
-import com.github.paolorotolo.appintro.ISlidePolicy
+import com.github.appintro.SlideBackgroundColorHolder
+import com.github.appintro.SlidePolicy
 import com.google.android.material.snackbar.Snackbar
 import fr.twentynine.keepon.R
 import fr.twentynine.keepon.intro.IntroActivity.Companion.COLOR_SLIDE_PERM
 import fr.twentynine.keepon.utils.KeepOnUtils
 
 
-class IntroFragmentPermission : Fragment(), ISlideBackgroundColorHolder, ISlidePolicy {
+class IntroFragmentPermission : Fragment(), SlideBackgroundColorHolder, SlidePolicy {
 
     private lateinit var mContext: Context
     private lateinit var mView: View
     private lateinit var mButton: Button
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        mContext = context!!
+        mContext = requireContext()
         mView = inflater.inflate(R.layout.fragment_intro_button, container, false)
 
         setBackgroundColor(defaultBackgroundColor)
 
         mButton = mView.findViewById(R.id.button)
-        mButton.setBackgroundColor(Color.parseColor(KeepOnUtils.darkerColor(COLOR_SLIDE_PERM, 0.4f)))
+        mButton.setBackgroundColor(KeepOnUtils.darkerColor(COLOR_SLIDE_PERM, 0.4f))
         mButton.text = getString(R.string.dialog_permission_button)
         mButton.setOnClickListener {
             val intent = Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS)
                 .setData(Uri.parse("package:" + mContext.packageName))
-                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                //.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                 .addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
                 .addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
             mContext.startActivity(intent)
@@ -53,31 +52,34 @@ class IntroFragmentPermission : Fragment(), ISlideBackgroundColorHolder, ISlideP
         val mImage = mView.findViewById<ImageView>(R.id.image)
         mImage.setImageResource(R.mipmap.img_intro_perm)
 
+        if (Settings.System.canWrite(requireContext().applicationContext))
+            mButton.visibility = View.INVISIBLE
+        else
+            mButton.visibility = View.VISIBLE
+
         return mView
     }
 
     override fun onResume() {
         super.onResume()
-        if (Settings.System.canWrite(context!!.applicationContext))
+        if (Settings.System.canWrite(requireContext().applicationContext))
             mButton.visibility = View.INVISIBLE
         else
             mButton.visibility = View.VISIBLE
     }
 
+    override val isPolicyRespected: Boolean
+        get() = Settings.System.canWrite(requireContext().applicationContext)
+
     override fun onUserIllegallyRequestedNextPage() {
         return Snackbar.make(mView, getString(R.string.intro_toast_permission_needed), Snackbar.LENGTH_LONG).show()
     }
 
-    override fun isPolicyRespected(): Boolean {
-        return Settings.System.canWrite(context!!.applicationContext)
-    }
+    override val defaultBackgroundColor: Int
+        get() = COLOR_SLIDE_PERM
 
     override fun setBackgroundColor(backgroundColor: Int) {
         val constraintLayout = mView.findViewById<ConstraintLayout>(R.id.main)
         constraintLayout.setBackgroundColor(backgroundColor)
-    }
-
-    override fun getDefaultBackgroundColor(): Int {
-        return Color.parseColor(COLOR_SLIDE_PERM)
     }
 }
