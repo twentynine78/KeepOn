72a98436fbc60d494a635c3e980796e1157d0e70
TwentyNine78 <twentynine78@protonmail.com>
2020-10-10 15:57:44 +0200

    Reduced use of the Glide cache memory

--- app/src/main/java/fr/twentynine/keepon/glide/GlideModule.kt
+++ app/src/main/java/fr/twentynine/keepon/glide/GlideModule.kt
@@ -8,13 +8,25 @@ import com.bumptech.glide.Registry
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.load.DecodeFormat
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.bitmap_recycle.LruBitmapPool
+import com.bumptech.glide.load.engine.cache.LruResourceCache
+import com.bumptech.glide.load.engine.cache.MemorySizeCalculator
 import com.bumptech.glide.module.AppGlideModule
 import com.bumptech.glide.request.RequestOptions
 
 @GlideModule
 class GlideModule : AppGlideModule() {
     override fun applyOptions(context: Context, builder: GlideBuilder) {
+        val calculator = MemorySizeCalculator.Builder(context)
+            .setMemoryCacheScreens(1f)
+            .setBitmapPoolScreens(1f)
+            .setLowMemoryMaxSizeMultiplier(0.2f)
+            .setMaxSizeMultiplier(0.2f)
+            .build()
+
         builder
+            .setMemoryCache(LruResourceCache(calculator.memoryCacheSize.toLong()))
+            .setBitmapPool(LruBitmapPool(calculator.bitmapPoolSize.toLong()))
             .setDefaultRequestOptions(
                 RequestOptions()
                     .format(DecodeFormat.PREFER_RGB_565)
