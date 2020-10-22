28ed5077491206c270034dc685c39ed257fc43ad
TwentyNine78 <twentynine78@protonmail.com>
2020-10-22 21:09:09 +0200

    Cleaned repository

--- appintro/build/generated/source/buildConfig/release/com/github/appintro/BuildConfig.java
+++ appintro/build/generated/source/buildConfig/release/com/github/appintro/BuildConfig.java
@@ -7,6 +7,4 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String LIBRARY_PACKAGE_NAME = "com.github.appintro";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 20;
-  public static final String VERSION_NAME = "6.0.0";
 }
