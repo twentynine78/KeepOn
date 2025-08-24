9fce366f5d4e2135d8a426e6239eecb966b5eca6
TwentyNine78 <twentynine78@protonmail.com>
2025-08-25 01:50:35 +0200

    Updated version to 2.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "fr.twentynine.keepon"
         minSdk = 28
         targetSdk = 36
-        versionCode = 23
-        versionName = "2.0.3"
+        versionCode = 24
+        versionName = "2.0.4"
 
         vectorDrawables {
             useSupportLibrary = true
