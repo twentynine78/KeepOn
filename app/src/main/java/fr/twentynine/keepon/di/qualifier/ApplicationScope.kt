f80905bb151e8af9c33d64238c57da0f79e218fc
TwentyNine78 <twentynine78@protonmail.com>
2026-06-08 01:30:51 +0200

    Fix the ApplicationScope KDoc to say Dispatchers.Default

--- app/src/main/java/fr/twentynine/keepon/di/qualifier/ApplicationScope.kt
+++ app/src/main/java/fr/twentynine/keepon/di/qualifier/ApplicationScope.kt
@@ -4,7 +4,7 @@ import javax.inject.Qualifier
 
 /**
  * Qualifies the application-lifetime [kotlinx.coroutines.CoroutineScope] (a singleton
- * backed by a [kotlinx.coroutines.SupervisorJob] on [kotlinx.coroutines.Dispatchers.IO]).
+ * backed by a [kotlinx.coroutines.SupervisorJob] on [kotlinx.coroutines.Dispatchers.Default]).
  * Injected wherever background work must outlive the calling component.
  */
 @Qualifier
